4757d7eb7c2b1841879ab96571e56c2ebd983a0e
PatrykFedex <narewski.patryk01@gmail.com>
2025-09-26 12:52:36 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+Pliki do gry Metin2 2D Online (multiplayer z Firebase).
+W pliku firebase-config.js masz ustawiony config do projektu mt2test.
+Wrzuć index.html, game.js i firebase-config.js na GitHub Pages.
---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyAKwaR7Pwax83vBxYetWYlQEo9ep0cS6As",
+  authDomain: "mt2test-cc1a5.firebaseapp.com",
+  databaseURL: "https://mt2test-cc1a5-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "mt2test-cc1a5",
+  storageBucket: "mt2test-cc1a5.appspot.com",
+  messagingSenderId: "543060114897",
+  appId: "1:543060114897:web:b0818476499e620083b4f0",
+  measurementId: "G-6YNZCQ5HN2"
+};
